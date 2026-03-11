7529916428c570a2ef9ae9951401108348bb619e
Grace Lipika Sakiay <gracelipikasakilay@gmail.com>
2026-03-12 00:55:08 +0530

    getting started

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Home from './pages/student/Home'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+ export const AppContext = createContext()
+
+ export const AppContextProvider = ()=>{
+
+    const value = {
+
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.childern}
+        </AppContext.Provider>
+    )
+ }
